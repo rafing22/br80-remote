ea314d8545d3ac7138616f2dca8c9d92986b239a
rafing22 <raffaele.marino0@gmail.com>
2026-09-02 12:10:11 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,36 +1,41 @@
 plugins {
-id("com.android.application")
-id("org.jetbrains.kotlin.android")
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
-namespace = "com.br80.remote"
-compileSdk = 34
+    namespace = "com.br80.remote"
+    compileSdk = 34
 
-defaultConfig {
-applicationId = "com.br80.remote"
-minSdk = 26
-targetSdk = 34
-versionCode = 1
-versionName = "0.1"
-}
+    defaultConfig {
+        applicationId = "com.br80.remote"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "0.1"
+    }
 
-buildTypes {
-debug {
-isMinifyEnabled = false
-}
-}
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+        }
+    }
 
-compileOptions {
-sourceCompatibility = JavaVersion.VERSION_17
-targetCompatibility = JavaVersion.VERSION_17
-}
-kotlinOptions {
-jvmTarget = "17"
-}
+    // Abilita il View Binding
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
-implementation("androidx.core:core-ktx:1.13.1")
-implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
 }
